296d0421a82859362ab83d87c93e840474dd6bf9
nicdavidson <ndavidson1717@gmail.com>
2025-03-14 17:29:05 +0000

    Add DbSchemaExtras trait to DbSchemaResource class to fix missing schema methods

--- src/Resources/DbSchemaResource.php
+++ src/Resources/DbSchemaResource.php
@@ -4,6 +4,7 @@ namespace DreamFactory\Core\Database\Resources;
 
 use DreamFactory\Core\Components\DataValidator;
 use DreamFactory\Core\Database\Components\TableDescriber;
+use DreamFactory\Core\Database\Components\DbSchemaExtras;
 use DreamFactory\Core\Database\Enums\DbFunctionUses;
 use DreamFactory\Core\Database\Enums\FunctionTypes;
 use DreamFactory\Core\Database\Schema\ColumnSchema;
@@ -28,7 +29,7 @@ use DreamFactory\Core\Resources\BaseRestResource;
 
 class DbSchemaResource extends BaseRestResource
 {
-    use DataValidator, TableDescriber;
+    use DataValidator, TableDescriber, DbSchemaExtras;
 
     //*************************************************************************
     //	Constants
